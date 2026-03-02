a653f54713744f6bfc87cd821ccc527d09d56931
EvgeniyPusser <evggeniipusser88@gmail.com>
2026-03-02 18:01:18 +0200

    без ай ди потреактовски исправили статс

--- shmitter/src/components/Stats.jsx
+++ shmitter/src/components/Stats.jsx
@@ -1,31 +1,12 @@
 import Avatar from "./Avatar.jsx";
-import {useContext, useEffect} from "react";
+import {useContext} from "react";
 import {ShmitterContext} from "../utils/context.js";
 
-const FOLLOWERS_ID = 'followers';
-const FOLLOWING_ID = 'following';
 const FOLLOWERS_LABEL = 'Followers';
 const FOLLOWING_LABEL = 'Following';
 
 const Stats = () => {
     const {user, stats, increaseFollowers, increaseFollowing} = useContext(ShmitterContext);
-
-    useEffect(() => {
-        const followersElement = document.getElementById(FOLLOWERS_ID);
-        const followingElement = document.getElementById(FOLLOWING_ID);
-
-        if (!followersElement || !followingElement) {
-            return;
-        }
-
-        followersElement.addEventListener('click', increaseFollowers);
-        followingElement.addEventListener('click', increaseFollowing);
-
-        return () => {
-            followersElement.removeEventListener('click', increaseFollowers);
-            followingElement.removeEventListener('click', increaseFollowing);
-        };
-    }, [increaseFollowers, increaseFollowing]);
 
     return (
         <div className={'user-stats'}>
@@ -34,8 +15,8 @@ const Stats = () => {
                 {user.name}
             </div>
             <div className={'stats'}>
-                <div id={FOLLOWERS_ID}>{FOLLOWERS_LABEL}: {stats.followers}</div>
-                <div id={FOLLOWING_ID}>{FOLLOWING_LABEL}: {stats.following}</div>
+                <div onClick={increaseFollowers}>{FOLLOWERS_LABEL}: {stats.followers}</div>
+                <div onClick={increaseFollowing}>{FOLLOWING_LABEL}: {stats.following}</div>
             </div>
         </div>
     )
